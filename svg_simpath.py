57ebd6c68e0206ee0a983bcba5a2a278ce72f47a
Terry N. Brown <terry_n_brown@yahoo.com>
2016-12-09 16:37:11 -0600

    discovered there are 'zM' sequences in paths

--- svg_simpath.py
+++ svg_simpath.py
@@ -20,6 +20,12 @@ from xml.sax.xmlreader import AttributesImpl
 PATHSPLIT = re.compile(r'([MLz])')
 WHITESPACE = re.compile(r'[ \t\n]')
 
+def flint(x):
+    return int(float(x))
+
+def coord_split(s):
+    return map(flint, s.split(','))
+
 class PathSimplify(XMLFilterBase):
 
     def startElement(self, name, attrs):
@@ -31,18 +37,35 @@ class PathSimplify(XMLFilterBase):
             assert not points[0], points[0]
             del points[0]
             if not points[-1]:
-                del points[1]
+                del points[-1]
             closed = points[-1] == 'z'
             if closed:
                 del points[-1]
             if len(points) > 50:
-                sys.stderr.write("%s\n" % len(points))
-                points = reduce(operator.add, 
-                    [points[i:i+2] for i in range(0,len(points),10)])
+                sys.stderr.write("%s ->" % (len(points)/2))
+                # points = reduce(operator.add, 
+                #     [points[i:i+2] for i in range(0,len(points),10)])
+                
+                new_points = points[:2]
+                coord = coord_split(points[1])
+                for idx in range(2, len(points), 2):
+                    try:
+                        new_coord = coord_split(points[idx+1])
+                    except:
+                        sys.stderr.write("%s\n" % d[:50])
+                        sys.stderr.write("%s\n" % d[-50:])
+                        sys.stderr.write("%s\n" % points[:8])
+                        sys.stderr.write("%s\n" % points[-8:])
+                        raise
+                    if new_coord != coord:
+                        coord = new_coord
+                        new_points.extend(points[idx:idx+2])
+                
+                sys.stderr.write("%s\n" % (len(new_points)/2))
                 if closed:
-                    points.append('z')
+                    new_points.append('z')
                 attrs = dict(attrs)
-                attrs['d'] = ''.join(points)
+                attrs['d'] = ''.join(new_points)
         return XMLFilterBase.startElement(self, name, AttributesImpl(attrs))
 
 def main():
